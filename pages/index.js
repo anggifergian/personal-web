2dd2bb868b53e7315a457f9b4b2556a8ef55c811
anggifergian <anggyfergian56@gmail.com>
2022-10-01 12:45:03 +0700

    Update UI

--- pages/index.js
+++ pages/index.js
@@ -77,7 +77,7 @@ export default function Home() {
         name: 'CMS DMS',
         desc: 'Backoffice for maintaining advertising in Jakarta, both content and location.',
         imagePath: '/images/cms-dms.png',
-        link: 'https://github.com/anggifergian/cms-dss',
+        link: 'http://31.220.6.203:3000/',
       },
       {
         key: 'micanid',
@@ -118,7 +118,7 @@ export default function Home() {
         <div className='flex justify-end pt-6 py-4'>
           <button
             onClick={() => setTheme(currentTheme === 'dark' ? 'light' : 'dark')}
-            className='px-2 py-2 border border-slate-300 rounded-xl bg-transparent hover:bg-slate-100 text-slate-500 dark:text-white dark:bg-transparent dark:hover:bg-slate-900'
+            className='px-2 py-2 border border-slate-300 rounded-xl bg-transparent hover:bg-slate-100 text-slate-500 dark:text-white dark:bg-transparent dark:hover:bg-slate-900 transition duration-200 ease-in-out'
           >
             {theme === 'dark'
               ? <SunIcon className='h-6 w-6' />
@@ -151,8 +151,9 @@ export default function Home() {
           <div className='grid grid-cols-1 md:grid-cols-2 gap-6 gap-y-8'>
             {state.projects.map(item => (
               <div key={item.key}>
-                <div className='mb-2 overflow-hidden rounded-lg dark:border-0 border-2 border-slate-200 border-opacity-40 hover:shadow-lg hover:border-opacity-0 transition ease-in-out duration-300'>
+                <div className='mb-3 overflow-hidden rounded-lg dark:border-0 border-2 border-slate-200 border-opacity-40 hover:shadow-lg hover:border-opacity-0 transition ease-in-out duration-300'>
                   <Image
+                    priority={item.key === 'galaxy-store'}
                     alt={item.key}
                     src={item.imagePath}
                     height={300}
@@ -161,7 +162,7 @@ export default function Home() {
                     className='w-full object-cover scale-1 hover:scale-110 transition ease-in-out duration-300'
                   />
                 </div>
-                <h3 className='mb-2 font-bold text-slate-800 dark:text-white'>
+                <h3 className='mb-1 font-bold text-slate-800 dark:text-white'>
                   <a target="_blank" href={item.link} rel="noopener noreferrer" className='hover:text-blue-500'>
                     {item.name}
                   </a>
