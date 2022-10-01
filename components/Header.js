ea02f2fcc089e076ae9464074f7ca90f1f6e57ee
anggifergian <anggyfergian56@gmail.com>
2022-10-01 12:44:54 +0700

    Add google analytics

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,34 @@
 import Head from 'next/head'
+import Script from 'next/script'
 
 const Header = ({ title = 'Anggi Fergian' }) => (
-  <Head>
-    <meta charSet='utf-8' />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="description" content="Anggi Fergian Pratama" />
+  <>
+    <Script
+      id='gtag-source'
+      strategy='lazyOnload'
+      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+    />
 
-    <title>{title}</title>
-    <link rel="icon" href="/af-logo.ico" />
-  </Head>
+    <Script id='gtag-dataLayer' strategy='lazyOnload'>
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          page_path: window.location.pathname,
+        });
+      `}
+    </Script>
+
+    <Head>
+      <meta charSet='utf-8' />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content="Anggi Fergian Pratama" />
+
+      <title>{title}</title>
+      <link rel="icon" href="/af-logo.ico" />
+    </Head>
+  </>
 )
 
 export default Header
